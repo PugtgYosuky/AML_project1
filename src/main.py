53ca499473df242cf0b0d44a2191554e24279a85
Joana Simões <joanasimoes04@gmail.com>
2023-03-08 16:10:38 +0000

    pipeline test Co-authored-by: Sircarrasco <Sircarrasco@users.noreply.github.com>

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+
+plt.rcParams["figure.figsize"] = (20,12)
+plt.rcParams['axes.grid'] = True
+plt.style.use('fivethirtyeight')
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['axes.facecolor'] = 'white'
+plt.rcParams['lines.linewidth'] = 3
