b485e8666e11467cbfe094732b47a4739407ae86
Joana Simões <joanasimoes04@gmail.com>
2023-03-09 14:50:36 +0000

    Add script w/ preprocessing pipeline

--- src/main.py
+++ src/main.py
@@ -3,7 +3,18 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+import os
+import sys
+import datetime
+import json
+
+from utils import *
+
+from sklearn.model_selection import train_test_split, cross_validate, KFold
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import VarianceThreshold, SelectKBest
 
 
 plt.rcParams["figure.figsize"] = (20,12)
@@ -12,3 +23,79 @@ plt.style.use('fivethirtyeight')
 plt.rcParams['figure.facecolor'] = 'white'
 plt.rcParams['axes.facecolor'] = 'white'
 plt.rcParams['lines.linewidth'] = 3
+
+if __name__ == '__main__':
+    # get experience
+    experiences = os.listdir(os.path.join('logs'))
+    if len(experiences) == 0:
+        exp = 'exp00'
+    else:
+        try:
+            num = int(sorted(experiences)[-1][3:]) + 1
+            if num < 10:
+                exp = f'exp0{num}'
+            else:
+                exp = f'exp{num}'
+        except:
+            exp = f'exp{datetime.datetime()}'
+
+    LOGS_PATH = os.path.join('logs', exp)
+    os.makedirs(LOGS_PATH)
+
+    # read config
+    config_path = sys.argv[1]
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    # save config
+    with open(os.path.join(LOGS_PATH, 'config.json'), 'w') as outfile:
+        json.dump(config, outfile)
+
+    # read data
+    data = pd.read_csv(config['dataset'])    
+
+    # remove dups
+    data.drop_duplicates(inplace=True)
+
+    # preprocess data
+    X = data.copy()
+    y = X.pop('attack_type')
+    # dict with classes map
+    classes_map = {
+        'normal' : 0,
+        'Dos' : 1,
+        'R2L' : 2,
+        'U2R' : 3,
+        'Probe' : 4
+    }
+    y = y.map(classes_map)
+
+    # split in train - test
+    x_train, x_test, y_train, y_test = train_test_split(X, y)
+
+    # set parameters
+    if config.get('norm_model', 'Standard') == 'MinMax':
+        norm_model = MinMaxScaler()
+    else:
+        norm_model = StandardScaler()
+
+    # columns types
+    categorical_columns = X.select_dtypes(include='object').columns.tolist()
+    numerical_columns = [col for col in X if col not in categorical_columns]
+    
+    pipeline = Pipeline(steps=[
+        ('categories', ColumnTransformer(
+            transformers=[
+                ('cat', CustomEncoder(), categorical_columns),
+                ('num', DropUniqueColumns(), numerical_columns)
+            ]
+        )),
+        ('feature_selection', SelectKBest(k=config.get('number_best_features', X.shape[1]))),
+        ('variance_selct', VarianceThreshold(threshold=config.get('variance_threshold', 0))),
+        ('scaler', norm_model)
+    ])
+
+    res = pipeline.fit_transform(x_train, y_train)
+    
+    # test models
+    print(res.shape)
+    print(x_train.shape)
---
+++ src/utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+class DropUniqueColumns(BaseEstimator):
+    def __init__(self, threshold=0.8):
+        self.threshold = threshold
+    def fit(self, X, y=None):
+        # drop columns with just one value
+        X = pd.DataFrame(X)
+        self.cols_to_drop = X.columns[X.nunique() == 1]
+        # correlation = X.corr(numeric_only=True)
+        # correlated_features = correlation.abs() > self.threshold
+        return self
+
+    def transform(self, X, y=None):
+        X = pd.DataFrame(X)
+        preprocessed = X.drop(self.cols_to_drop, axis=1)
+        return preprocessed
+
+
+class CustomEncoder(BaseEstimator):
+    def __init__(self, cols=None):
+        self.cols = cols
+
+    def fit(self, X, y=None):
+        self.encoder = LabelEncoder()
+        self.encoders = {}
+        for col in X:
+            self.encoders[col] = LabelEncoder().fit(X[col])
+        return self
+
+    def transform(self, X, y=None):
+        X_encoded = X.copy()
+        for col in X:
+            X_encoded[col] = self.encoders[col].transform(X[col])
+
+        return pd.DataFrame(X_encoded, columns=X.columns)
+
